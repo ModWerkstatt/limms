1530090e983591d18ab8681e0e54108116170f10
Florian Thiel <flth@flth.de>
2023-11-05 12:58:59 +0100

    initial von jay

---
+++ strings.lua
@@ -0,0 +1,30 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Special wagon for glass transport (Limms)",
+		["mod_desc"] = "Increasing environmental pollution and the high demand for mobility in Europe have led to a search for ways to shift freight traffic from roads to rail. Until now, large-format flat glass in Europe has mainly been transported by special trucks. However, AStrans and Pilkington Inc. have developed an innovative solution using their own special wagons with special fixtures and a non-slip floor to transport flat glass by rail. These wagons are environmentally friendly and enable the efficient transport of flat glass between Scandinavia, England and mainland Europe. For this innovation, AStrans received the European Transport Award in 1998.",
+		["gedreht"] = "(rotated) ",
+		["sound_check"] = "Activate custom sound?",
+		["height"] = "Height",
+		["offset x-axis"] = "Offset x-axis",
+		["ground"] = "Ground",
+		["rail"] = "Rail",
+		["vehicle"] = "Vehicle",
+
+	},
+	de = {
+
+		["mod_name"] = "Spezialwagen für Glastransport (Limms)",
+		["mod_desc"] = "Die steigende Umweltbelastung und der hohe Mobilitätsbedarf in Europa haben dazu geführt, dass nach Möglichkeiten gesucht wurde, den Frachtverkehr von Straßen auf die Schiene zu verlagern. Bisher wurde großformatiges Flachglas in Europa hauptsächlich mit speziellen Lastwagen transportiert. AStrans und Pilkington Inc. haben jedoch eine innovative Lösung entwickelt, bei der eigene Spezialwaggons mit speziellen Vorrichtungen und einem rutschfesten Boden für den Transport von Flachglas auf der Schiene verwendet werden. Diese Waggons sind umweltfreundlich und ermöglichen den effizienten Transport von Flachglas zwischen Skandinavien, England und dem europäischen Festland. Für diese Innovation erhielt AStrans 1998 den Europäischen Transportpreis.",
+		["gedreht"] = "(gedreht) ",
+		["sound_check"] = "Eigenes Soundset aktivieren?",
+		["height"] = "Höhe",
+		["offset x-axis"] = "Verschub X-Achse",
+		["ground"] = "Boden",
+		["rail"] = "Gleis",
+		["vehicle"] = "Fahrzeug",
+
+	},
+}
+end
